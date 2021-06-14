b5a1c82c591f9fc4e5840eab6ad0640d6fc0dcdf
tombo <thulata@pixelfederation.com>
2021-06-14 15:33:09 +0200

    squashed + TODOS

---
+++ src/tech/pxfd/Utils.groovy
@@ -0,0 +1,63 @@
+package tech.pxfd
+
+@Grab("org.yaml:snakeyaml:1.29")
+import org.yaml.snakeyaml.Yaml
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+//check this if want to loop inside resources without knowing file names
+// https://stackoverflow.com/questions/61088491/how-to-access-to-a-static-file-in-resources-folder-of-the-shared-library-from-wi
+
+class Utils {
+    def context
+
+    Utils(pipelineContext) {
+        this.context = pipelineContext
+    }
+
+    Map getResourcePod(String podName) {
+        return new Yaml().load(this.context.libraryResource("pod/${podName}.yaml"))
+    }
+
+    Map getResourceContainer(String containerName) {
+        return new Yaml().load(this.context.libraryResource("containers/${containerName}.yaml"))
+    }
+
+    public static final Map mapDeepCopy(Map originalMap) {
+        return new JsonSlurper().parseText(JsonOutput.toJson(originalMap))
+    }
+
+    public static final Map removeUnmergable(Map m) {
+        m.remove('names')
+        m.remove('tag')
+        m.remove('_inherit')
+        return m
+    }
+
+    public static final Map mergeContainers(Map left, Map right) {
+        return right.inject(left.clone()) { map, entry ->
+            if (map[entry.key] instanceof Map && entry.value instanceof Map) {
+                map[entry.key] = this.mergeContainers(map[entry.key], entry.value)
+            } else if (map[entry.key] instanceof Collection && entry.value instanceof Collection) {
+                map[entry.key] += entry.value
+            } else {
+                map[entry.key] = entry.value
+            }
+            return map
+        }
+    }
+
+    public static final String mapToYaml(Map m) {
+        return new Yaml().dump(m)
+    }
+}
+
+/*
+import jenkins.plugins.git.GitSCMSource;
+modernSCM(
+    [
+      $class: 'GitSCMSource',
+      remote: 'https://github.com/your-company/demo-library.git',
+      credentialsId: 'github'
+    ]
+*/
--- vars/getTemplate.groovy
+++ vars/getTemplate.groovy
@@ -1,4 +1,52 @@
-def call(Map config = [:]) {
-  def resourceContent = mainScript.libraryResource('tombo.test')
-  println(resourceContent)
+import tech.pxfd.Utils
+
+//TODO provide simpler env definition then {name: CONFD_EXECUTE, value: 'true'}
+// implement fixEnv function, and accept en vars in simpler eg ["CONFD_EXECUTE":"true"] notation
+
+//TODO add suport for dot separeted nested keys like spec.resouces.requests.memory
+
+//TODO if provided key not match know template, will crash, allow definitions for unkown containers
+
+String call(Map config = [:]) {
+  def utils = new Utils(this)
+  Map podTemplate = utils.getResourcePod('default')
+
+  podTemplate['spec']['containers'] = []
+
+  Map containersFinalDef = [:]
+
+  clones = [:]
+
+  config.each{String containerName, Map containerParams ->
+
+    Map baseResourceFromLibTemplate = utils.getResourceContainer(containerName)
+    
+    containerParams.each{ String key, def value ->
+      switch(key) {
+        case "tag":
+          baseResourceFromLibTemplate["image"] = baseResourceFromLibTemplate["image"].split(":")[0] + ":" + value
+        break
+        case "names":
+          value.each{ cname -> 
+            clones[cname] = [:]
+            clones[cname]["_inherit"] = containerName
+          }
+        break
+      }
+    }
+
+    Map paramsToMerge = Utils.removeUnmergable(containerParams)
+    Map merge = Utils.mergeContainers( baseResourceFromLibTemplate , paramsToMerge)
+    podTemplate['spec']['containers'] += merge
+    containersFinalDef[containerName] = merge
+  }
+
+  clones.each{String containerName, Map containerParams ->
+    Map params = Utils.mapDeepCopy(containersFinalDef[containerParams["_inherit"]])
+    params["name"] = containerName
+    podTemplate['spec']['containers'] += params
+    containersFinalDef[containerName] = params
+  }
+
+  return Utils.mapToYaml(podTemplate)
 }
