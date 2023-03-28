dd2e78de0edc8da547e9fb7a5583bace46bb5b8b
tombokombo <tombo@sysart.tech>
2023-03-28 14:30:19 +0200

    add test for names

--- src/tech/pxfd/GetTemplate.groovy
+++ src/tech/pxfd/GetTemplate.groovy
@@ -69,9 +69,13 @@ class GetTemplate {
                 baseResourceFromLibTemplate["image"] = baseResourceFromLibTemplate["image"].split(":")[0] + ":" + value
                 break
                 case "names":
-                value.each{ String cname ->
-                    clones[cname] = [:]
-                    clones[cname]["_inherit"] = containerName
+                if ( value instanceof List ) {
+                    value.each{ String cname ->
+                        clones[cname] = [:]
+                        clones[cname]["_inherit"] = containerName
+                    }
+                } else {
+                    utils.error("`names` must be a list")
                 }
                 break
                 case "env":
