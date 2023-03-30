f8aa083d00eaf3ca18cb67133cdbf4a747cc6055
tombokombo <tombo@sysart.tech>
2023-03-30 23:32:57 +0200

    fix containers init

--- vars/getTemplate.groovy
+++ vars/getTemplate.groovy
@@ -13,7 +13,12 @@ String call(Map conatinersConf = [:], Map podConf = [:]) {
 
 String Jnlp(Map conatinersConf = [:], Map podConf = [:]) {
   // just define keys, props are from resources/container/{key} templates
-  conatinersConf['jnlp'] = [:]
+  List containerList = ['jnlp']
+  containerList.each { c ->
+    if(!conatinersConf.containsKey(c)) {
+      conatinersConf[c] = [:]
+    }
+  }
 
   def template = new GetTemplate(this, conatinersConf, podConf)
   return template.render()
@@ -21,65 +26,72 @@ String Jnlp(Map conatinersConf = [:], Map podConf = [:]) {
 
 String Util(Map conatinersConf = [:], Map podConf = [:]) {
   // just define keys, props are from resources/container/{key} templates
-  conatinersConf['jnlp'] = [:]
-  conatinersConf['util'] = [:]
-
+  List containerList = ['jnlp', 'util']
+  containerList.each { c ->
+    if(!conatinersConf.containsKey(c)) {
+      conatinersConf[c] = [:]
+    }
+  }
   def template = new GetTemplate(this, conatinersConf, podConf)
   return template.render()
 }
 
 String UtilKaniko(Map conatinersConf = [:], Map podConf = [:]) {
   // just define keys, props are from resources/container/{key} templates
-  conatinersConf['jnlp'] = [:]
-  conatinersConf['util'] = [:]
-  // for ecr helper, we have optional configmap defined on pod template
-  // if configmap not present, empty mount path will be created on kaniko container - /kaniko/.docker
-  conatinersConf['kaniko'] = [:]
-
+  List containerList = ['jnlp', 'util', 'kaniko']
+  containerList.each { c ->
+    if(!conatinersConf.containsKey(c)) {
+      conatinersConf[c] = [:]
+    }
+  }
   def template = new GetTemplate(this, conatinersConf, podConf)
   return template.render()
 }
 
 String UtilKanikoPhp(Map conatinersConf = [:], Map podConf = [:]) {
   // just define keys, props are from resources/container/{key} templates
-  conatinersConf['jnlp'] = [:]
-  conatinersConf['util'] = [:]
-  // for ecr helper, we have optional configmap defined on pod template
-  // if configmap not present, empty mount path will be created on kaniko container - /kaniko/.docker
-  conatinersConf['kaniko'] = [:]
-  conatinersConf['php'] = [:]
-
+  List containerList = ['jnlp', 'util', 'kaniko', 'php']
+  containerList.each { c ->
+    if(!conatinersConf.containsKey(c)) {
+      conatinersConf[c] = [:]
+    }
+  }
   def template = new GetTemplate(this, conatinersConf, podConf)
   return template.render()
 }
 
 String UtilBuildx(Map conatinersConf = [:], Map podConf = [:]) {
   // just define keys, props are from resources/container/{key} templates
-  conatinersConf['jnlp'] = [:]
-  conatinersConf['util'] = [:]
-  conatinersConf['buildx'] = [:]
-
+  List containerList = ['jnlp', 'util', 'buildx']
+  containerList.each { c ->
+    if(!conatinersConf.containsKey(c)) {
+      conatinersConf[c] = [:]
+    }
+  }
   def template = new GetTemplate(this, conatinersConf, podConf)
   return template.render()
 }
 
 String UtilPhp(Map conatinersConf = [:], Map podConf = [:]) {
   // just define keys, props are from resources/container/{key} templates
-  conatinersConf['jnlp'] = [:]
-  conatinersConf['util'] = [:]
-  conatinersConf['php'] = [:]
-
+  List containerList = ['jnlp', 'util', 'php']
+  containerList.each { c ->
+    if(!conatinersConf.containsKey(c)) {
+      conatinersConf[c] = [:]
+    }
+  }
   def template = new GetTemplate(this, conatinersConf, podConf)
   return template.render()
 }
 
 String UtilBuildxPhp(Map conatinersConf = [:], Map podConf = [:]) {
   // just define keys, props are from resources/container/{key} templates
-  conatinersConf['jnlp'] = [:]
-  conatinersConf['util'] = [:]
-  conatinersConf['buildx'] = [:]
-  conatinersConf['php'] = [:]
-
+  List containerList = ['jnlp', 'util', 'buildx', 'php']
+  containerList.each { c ->
+    if(!conatinersConf.containsKey(c)) {
+      conatinersConf[c] = [:]
+    }
+  }
   def template = new GetTemplate(this, conatinersConf, podConf)
   return template.render()
 }
